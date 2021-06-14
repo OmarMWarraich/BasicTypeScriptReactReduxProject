1636c2782b4479c69c8e5af226f2ba77de13792d
OmarMWarraich <earthling986@gmail.com>
2021-06-14 16:34:13 -0400

    Create products module adding prodicts, cart and loading props

---
+++ src/redux/modules/products.ts
@@ -0,0 +1,83 @@
+// TODO: We should move typedAction elsewhere since it's shared
+
+import { typedAction } from './users';
+import { Dispatch, AnyAction } from 'redux';
+
+type Product = {
+    id: number;
+    name: string;
+    price: number;
+    img: string;
+};
+
+type CartItem = {
+    id: number;
+    quantity: number;
+}
+
+type ProductState = {
+    products: Product[];
+    loading: boolean;
+    cart: CartItem[];
+};
+
+const initialState: ProductState = {
+    products: [],
+    loading: false,
+    cart: [],
+};
+
+const addProducts = (products: Product[]) => {
+    return typedAction('products/ADD_PRODUCTS', products);
+}
+
+export const addToCart = (product: Product, quantity: number) => {
+    return typedAction('products/ADD_TO_CART', {product, quantity});
+}
+
+// Action creator returning a thunk!
+export const loadProducts = () => {
+    return (dispatch: Dispatch<AnyAction>) => {
+        setTimeout(() => {
+            // Pretend to load an item
+            dispatch(
+                addProducts([
+                    {
+                    id: 1,
+                    name: 'Cool Headphones',
+                    price: 4999,
+                    img: 'https://placeimg.com/640/480/tech/5',
+                },
+                ])
+            );
+        }, 500);
+    };
+};
+
+type ProductAction = ReturnType<typeof addProducts | typeof addToCart>;
+
+export function productsReducer(
+    state= initialState,
+    action: ProductAction
+): ProductState {
+  switch (action.type) {
+      case 'products/ADD_PRODUCT':
+          return {
+              ...state,
+              products: [...state.products, ...action.payload]
+          };
+      case 'products/ADD_TO_CART':
+          return {
+              ...state,
+              cart: [
+                  ...state.cart,
+                  {
+                    id: action.payload.product.id,
+                    quantity: action.payload.quantity,
+                  },
+              ],
+          };
+        default:
+            return state;
+  }
+}
